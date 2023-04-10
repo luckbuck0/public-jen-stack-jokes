44248e4bf1a14725256f9f16de50ec133755aafd
Luckie Bah <bah966@gmail.com>
2023-04-10 19:48:16 +0100

    Created a get request to send the array from the server to the client side and put it on the dom using ajax and append

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,48 @@
 console.log('client.js sourced');
 
-$( document ).ready( onReady );
+$(document).ready(onReady);
 
 function onReady() {
+    $('#addJokeButton').on('click', addJoke)
+    showJokes()
     console.log('DOM ready');
 }
+
+function showJokes() {
+
+    $.ajax({
+        method: 'GET',
+        url: '/jokes',
+    }).then(function (getJokes) {
+        for (let joke of getJokes) {
+            // looping through the item
+            $('#outputDiv').append(`
+      <div>${joke.whoseJoke} </div>
+       <div>${joke.jokeQuestion}</div>
+       <div>${joke.punchLine}  </div>
+  `)
+        }
+        ;
+    })
+};
+
+function addJoke(){
+
+        console.log('this', $('.box').text())
+        // set the input value of .box to a variable
+        let answer = $('.box').html()
+        // creating the object to be sent over
+        let jokes = {
+            whoseJoke: $('#whoseJokeIn').val(),
+            jokeQuestion: $('#qustionIn').val(),
+            punchLine: $('#punchlineIn').val
+        }
+        // sending over the problem variable
+        $.ajax({
+            method: 'POST',
+            url: '/newJokes',
+            data: jokes
+        })
+    
+}
+
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require( 'body-parser' );
 const PORT = 5000;
 
+
 // use bodyParser.urlencoded throughout the app with this:
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -37,6 +38,21 @@ let jokes = [
 // serve back static files
 app.use(express.static('server/public'));
 
+
 app.listen(PORT, () => {
   console.log('server running on: ', PORT);
 }); // end spin up server
+
+
+// sending the jokes array to the client js
+app.get('/jokes', (req, res) => {
+
+  res.send(jokes);
+})
+
+app.post('/newJokes', (req, res) => {
+
+  let newJokes= req.body;
+  jokes.push(newJokes)
+  res.sendStatus(201)
+})
